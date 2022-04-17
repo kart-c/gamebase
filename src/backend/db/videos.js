9f6b5e5efc3ceb5013889e2bcbd81a971f999d58
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-17 19:45:35 +0530

    feat: add videos in database

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -4,18 +4,292 @@
  * */
 
 export const videos = [
-  {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
-  },
-  {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-  },
+	{
+		_id: 'hvoD7ehZPcM',
+		title: 'Grand Theft Auto V: The Official Trailer',
+		videoThumbnail: 'https://i3.ytimg.com/vi/68pYm_KgptU/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/r7mISTzNbcLem_plduc_SKMh9KUiP8CWnuhS7H4-CCErF4juXxkrK-5_4LOIMeYl-lXAXMUG=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'Rockstar Games',
+		time: '1:00',
+		viewCount: '19M views',
+		likes: '288K',
+		category: 'Action',
+		description:
+			'Los Santos: a sprawling metropolis full of self-help gurus, starlets and fading celebrities struggling to stay afloat in an era of economic uncertainty and cheap cable TV. Amidst the turmoil, three very different criminals risk everything in a series of daring and dangerous heists that could set them up for life.',
+	},
+	{
+		_id: 'eaW0tYpxyp0',
+		title: 'Red Dead Redemption 2: Official Trailer',
+		videoThumbnail: 'https://i3.ytimg.com/vi/eaW0tYpxyp0/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/r7mISTzNbcLem_plduc_SKMh9KUiP8CWnuhS7H4-CCErF4juXxkrK-5_4LOIMeYl-lXAXMUG=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'Rockstar Games',
+		time: '2:08',
+		viewCount: '35M views',
+		likes: '261K',
+		category: 'RPG',
+		description:
+			'After a robbery goes badly wrong in the western town of Blackwater, Arthur Morgan and the Van der Linde gang are forced to flee. With federal agents and the best bounty hunters in the nation massing on their heels, the gang must rob, steal and fight their way across the rugged heartland of America in order to survive. As deepening internal divisions threaten to tear the gang apart, Arthur must make a choice between his own ideals and loyalty to the gang who raised him.',
+	},
+	{
+		_id: '68pYm_KgptU',
+		title: 'God of War: Ragnarok - Official Gameplay Trailer',
+		videoThumbnail: 'https://i3.ytimg.com/vi/68pYm_KgptU/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/H_---ano_f27DOCejDhUdBMtBcxcv32MoXmFKQc_mBcxYSH4HPZTGuBUdm7UgI5VlutMOVX2=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'IGN',
+		time: '3:16',
+		viewCount: '377K views',
+		likes: '99K',
+		category: 'RPG',
+		description:
+			'From Santa Monica Studio comes the sequel to the critically acclaimed God of War (2018). Kratos and Atreus must journey to each of the Nine Realms in search of answers as they prepare for the prophesied battle that will end the world. Together, Kratos and Atreus venture deep into the Nine Realms in search of answers as Asgardian forces prepare for war.',
+	},
+	{
+		_id: 'TTaCrP_U4ao',
+		title: 'Age of Empires IV - Gameplay Trailer',
+		videoThumbnail: 'https://i3.ytimg.com/vi/TTaCrP_U4ao/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/Jde8By5poOpgWGjHPAnlTKU7z6GUefleFw0zght3TaS_rV-G8JpOqNnrU8R8-jqSb_sVazQvVQ=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'Xbox',
+		time: '5:54',
+		viewCount: '2.7M views',
+		likes: '56K',
+		category: 'Strategy',
+		description:
+			'One of the best-selling real-time strategy games returns in glory with Age of Empires IV, putting you at the center of epic historical battles that shaped the modern world.',
+	},
+	{
+		_id: 'Yxwgg35Bt5E',
+		title: 'Need For Speed Hot Pursuit 2010 PC Gameplay Part 4 Maximus Settings 720p HD',
+		videoThumbnail: 'https://i3.ytimg.com/vi/Yxwgg35Bt5E/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLQamZy3e49VBa3LXMdEx-U5zt9hGj2pEKpgh000xg=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'MrEdxwx',
+		time: '13:12',
+		viewCount: '1.5M views',
+		likes: '5.2K',
+		category: 'Racing',
+		description:
+			'Need for Speed Hot Pursuit developed studio Criterion Games,the creators of winning general acceptance racing series Burnout. Recognized innovators changing the fundamentals of the genre once again and present a project that brings together players and offers them not only compete with each other on the Internet,but also to perform the job of his friends.',
+	},
+	{
+		_id: '_pbBqHhmYbs',
+		title: 'Forza Horizon 5 - Koenigsegg Jesko | Goliath Race Gameplay',
+		videoThumbnail: 'https://i3.ytimg.com/vi/_pbBqHhmYbs/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLTqQsgrmKJi7hnWrDr9guEAJuNxh-XC4Gx1NGROuQ=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'DIZEUUL',
+		time: '11:25',
+		viewCount: '1.05M views',
+		likes: '15K',
+		category: 'Racing',
+		description:
+			'Goliath Race with a Koenigsegg Jesko full upgrades (Tuned) on Forza Horizon 5 vs Bugatti Divo vs Koenigsegg Agera RS vs Lotus Evija vs Ferrari FFX K vs Lamborghini Sesto Elemento FE',
+	},
+	{
+		_id: '8bnGGp1yZY8',
+		title: 'Sekiro | Genichiro Ashina Boss Fight [1440p | 60Fps]',
+		videoThumbnail: 'https://i3.ytimg.com/vi/8bnGGp1yZY8/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLTfQAqQ-XusAu_uV9lSBEXjUPCfsDBHxHWYKv6B=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'DIZEUUL',
+		time: '7:01',
+		viewCount: '173K views',
+		likes: '2.1K',
+		category: 'RPG',
+		description:
+			'Sorry for the weird static in the first cutscene, not sure what happened. Second time in 2nd phase so it wasnt really clean but well',
+	},
+	{
+		_id: 'WEwFpaL6X3w',
+		title: 'Sekiro Shadows Die Twice PS5 - Isshin The Sword Saint Boss Fight & Ending (4K 60FPS)',
+		videoThumbnail: 'https://i3.ytimg.com/vi/WEwFpaL6X3w/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLSkBLk27hPe6RV3Mqre2WLcxJU1GsC_PPaYnetA9A=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'Shirrako',
+		time: '11:27',
+		viewCount: '161K views',
+		likes: '1.6K',
+		category: 'RPG',
+		description: `Sekiro Shadows Die Twice Gameplay Walkthrough PS4 PRO No Commentary 1080p 60fps HD let's play playthrough review guide`,
+	},
+	{
+		_id: 'd74REG039Dk',
+		title: 'Grand Theft Auto 5 Gameplay (PS4 HD) [1080p]',
+		videoThumbnail: 'https://i3.ytimg.com/vi/d74REG039Dk/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLRFpJvq-wBohC02mHXTpx_uhRit3eV204GWWoiHbg=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'Throneful',
+		time: '13:45',
+		viewCount: '11M views',
+		likes: '65K',
+		category: 'Action',
+		description: `Grand Theft Auto V is an open world, action-adventure video game developed by Rockstar North and published by Rockstar Games. It was released on 17 September 2013 for the PlayStation 3 and Xbox 360, and on 18 November 2014 for the PlayStation 4 and Xbox One.`,
+	},
+	{
+		_id: 'wQATS4HOxdo',
+		title: 'Horizon Forbidden West - State of Play Gameplay Reveal | PS5',
+		videoThumbnail: 'https://i3.ytimg.com/vi/wQATS4HOxdo/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLSyRIOTnFGuNAWmraaNw-SQuv5r3HeRvlpRqGJe3cc=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'PlayStation',
+		time: '19:10',
+		viewCount: '9.89M views',
+		likes: '287K',
+		category: 'RPG',
+		description:
+			'Join Aloy as she braves the Forbidden West, a deadly frontier that conceals mysterious new threats.',
+	},
+	{
+		_id: '2AZmuZNu5LA',
+		title: 'Hogwarts Legacy - State of Play Official Gameplay Reveal | PS5, PS4',
+		videoThumbnail: 'https://i3.ytimg.com/vi/2AZmuZNu5LA/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLSyRIOTnFGuNAWmraaNw-SQuv5r3HeRvlpRqGJe3cc=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'PlayStation',
+		time: '14:43',
+		viewCount: '8.51M views',
+		likes: '306K',
+		category: 'Action',
+		description:
+			'Become the student you’ve always wanted to be as you experience life at Hogwarts and embark on a journey through the wizarding world. Discover magical beasts, craft potions, and master spell casting as you uncover the mystery of Ancient Magic. ',
+	},
+	{
+		_id: 'yAk_ZNbq0k4',
+		title: 'WW2 - Normandy Landings - D-Day - Call of Duty WW2',
+		videoThumbnail: 'https://i3.ytimg.com/vi/yAk_ZNbq0k4/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLQeQ4zPdEX480ehG9EPZ_-ECfKZcIXN173H_38=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'igcompany',
+		time: '22:46',
+		viewCount: '18M views',
+		likes: '135K',
+		category: 'Action',
+		description:
+			'This is a video of the Normandy Landings during D-Day, June 6, 1944 simulated in Call of Duty: WW2. It shows the American Army taking over the beach and the surroundings.',
+	},
+	{
+		_id: '0d-U2QL2Qvc',
+		title: 'Red Dead Redemption 2 - Combat & Open World Free Roam Gameplay',
+		videoThumbnail: 'https://i3.ytimg.com/vi/0d-U2QL2Qvc/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLR4vlvC0Zp-ecCCOcA1RhSoCWlWUvM7Cg5kAWHlGw=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'DKGames',
+		time: '12:05',
+		viewCount: '5.54M views',
+		likes: '42K',
+		category: 'RPG',
+		description:
+			'Red Dead Redemption 2 is a Western-themed action-adventure video game developed and published by Rockstar Games. The game is a prequel to the 2010 game Red Dead Redemption, and the third entry in the Red Dead series. Set in 1899, it follows the story of outlaw Arthur Morgan, a member of the Dutch Van der Linde gang.',
+	},
+	{
+		_id: '_lV2r7kORKs',
+		title: 'Civilization 6 Review',
+		videoThumbnail: 'https://i3.ytimg.com/vi/_lV2r7kORKs/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/H_---ano_f27DOCejDhUdBMtBcxcv32MoXmFKQc_mBcxYSH4HPZTGuBUdm7UgI5VlutMOVX2=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'IGN',
+		time: '4:13',
+		viewCount: '1.02M views',
+		likes: '9.4K',
+		category: 'Strategy',
+		description: 'Civilization VI reviewed by Dan Stapleton on PC.',
+	},
+	{
+		_id: 'ST5ZNfXgyYI',
+		title: 'Age of Empires IV - The Delhi Sultanate',
+		videoThumbnail: 'https://i3.ytimg.com/vi/ST5ZNfXgyYI/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/yqdsqtHzsz8Xp808Us6bgCTq36hPHJNZQSbCqNj7TW2YsCD4Vg7AMqvfVcdVenrlMAYaH4BN=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'Age of Empires',
+		time: '3:22',
+		viewCount: '141K views',
+		likes: '4.2K',
+		category: 'Strategy',
+		description:
+			'The Delhi Sultanate has a strong focus on research and defense, with an edge in technological advancement over other civilizations. Their army can be terrifying to encounter with the War Elephant, a melee unit of staggering raw strength capable of high damage, as the core of their army.',
+	},
+	{
+		_id: 'ICRuSQ3Ilkk',
+		title: 'Age of Empires 4 - 1v1 INTENSE BATTLES | Multiplayer Gameplay',
+		videoThumbnail: 'https://i3.ytimg.com/vi/ICRuSQ3Ilkk/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLRr-eJke28VPYGvvdrGUz6i1K_B3J2uV2rI6jPFXA=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'SergiuHellDragoonHQ',
+		time: '50:58',
+		viewCount: '914K views',
+		likes: '7.1K',
+		category: 'Strategy',
+		description: `Age of Empires IV is a real-time strategy video game developed by Relic Entertainment in partnership with World's Edge and published by Xbox Game Studios. It is the fourth installment of the Age of Empires series.`,
+	},
+	{
+		_id: 'aCek0vSQ2GE',
+		title: 'F1 2021 Gameplay (PS5 UHD) [4K60FPS]',
+		videoThumbnail: 'https://i3.ytimg.com/vi/aCek0vSQ2GE/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLRFpJvq-wBohC02mHXTpx_uhRit3eV204GWWoiHbg=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'Throneful',
+		time: '13:13',
+		viewCount: '600K views',
+		likes: '4.2K',
+		category: 'Racing',
+		description: 'F1 2021 Gameplay (PS5 UHD) [4K60FPS]',
+	},
+	{
+		_id: 'OiOXJIYkSXw',
+		title: 'Forza Horizon 5 4K Official Announcement Trailer',
+		videoThumbnail: 'https://i3.ytimg.com/vi/OiOXJIYkSXw/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLSuH0b9tVYmA1BFFYMMv9cOJ09om78i-3Ox9K9T3R4=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'GameSpot',
+		time: '1:55',
+		viewCount: '329K views',
+		likes: '6.9K',
+		category: 'Racing',
+		description:
+			'Check out the 4K Version of the Forza Horizon 5 announcement trailer that was shown off during the Xbox & Bethesda E3 2021 Showcase!',
+	},
+	{
+		_id: 'AJm-cwt0Ba8',
+		title:
+			'Need For Speed: Hot Pursuit on PS5 - 16 Minutes of Gameplay (Free Drive, Police Chases)',
+		videoThumbnail: 'https://i3.ytimg.com/vi/AJm-cwt0Ba8/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLSwqdRyw-QsdF0j3ACyUNMPgQrKcIOucfEO2sA0DQ=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'Racing Video Games',
+		time: '16:05',
+		viewCount: '480K views',
+		likes: '3.9K',
+		category: 'Racing',
+		description: `NFS Hot Pursuit Remastered on PlayStation 5! The PS4 Remaster of the PS3 game, played on PS5 via backwards compatibility. Free driving around Seacrest County's open world in a Koenigsegg Agera, before doing two Hot Pursuit events in the Porsche 918 Spyder and Bugatti Veyron`,
+	},
+	{
+		_id: 'j2j1RT5vJ8w',
+		title: 'Spider-Man PS5 - MCU Iron Spider Suit (4K)',
+		videoThumbnail: 'https://i3.ytimg.com/vi/j2j1RT5vJ8w/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLQUMCJYIY2hDpIkuPfBv0iLXb_oKwY_w_pSiktZCQ=s88-c-k-c0x00ffffff-no-rj',
+		channelName: 'Racing Video Games',
+		time: '08:36',
+		viewCount: '6.6M views',
+		likes: '56K',
+		category: 'RPG',
+		description: `Featuring the Iron Spider suit, this video will showcase some free roam gameplay of Marvel's Spider-Man PS5 in 4K.`,
+	},
+	{
+		_id: 'OwVe4ZNeQZk',
+		title: `E3 Cinematic Trailer - Assassin's Creed 4 Black Flag [UK]`,
+		videoThumbnail: 'https://i3.ytimg.com/vi/OwVe4ZNeQZk/maxresdefault.jpg',
+		channelThumbnail:
+			'https://yt3.ggpht.com/ytc/AKedOLT52k4kP9eMbrjlikSuNOY0ZKo1tjjVGuwhx72ulg=s88-c-k-c0x00ffffff-no-rj',
+		channelName: `Assassin's Creed UK`,
+		time: '03:53',
+		viewCount: '7.1M views',
+		likes: '72K',
+		category: 'Action',
+		description:
+			'You are Captain Edward Kenway, a pirate among assassins. Set sail for a unique adventure in the Caribbean where you will live by your own rules, explore uncharted islands and discover the gritty life of the legendary outlaws of the 18th century.',
+	},
 ];
