6e6277d343f8953a9a780a2cca98b141d108fc7a
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-22 11:52:37 +0530

    feat: add remove from likes case

--- src/reducer/likes-reducer.js
+++ src/reducer/likes-reducer.js
@@ -3,6 +3,9 @@ export const likesReducer = (state, { type, payload }) => {
 		case 'ADD_TO_LIKES':
 			return { ...state, likes: payload };
 
+		case 'REMOVE_FROM_LIKES':
+			return { ...state, likes: payload };
+
 		default:
 			throw new Error('NO CASE DEFINED IN LIKES REDUCER');
 	}
