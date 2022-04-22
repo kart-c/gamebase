ba3b689cab0a846b4b902be4000616b4a896be9b
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-22 11:26:56 +0530

    feat: add likes reducer

---
+++ src/reducer/likes-reducer.js
@@ -0,0 +1,9 @@
+export const likesReducer = (state, { type, payload }) => {
+	switch (type) {
+		case 'ADD_TO_LIKES':
+			return { ...state, likes: payload };
+
+		default:
+			throw new Error('NO CASE DEFINED IN LIKES REDUCER');
+	}
+};
