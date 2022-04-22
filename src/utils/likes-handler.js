c14096573a6a0dbd56071e5fef24f356f779cd78
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-22 11:28:26 +0530

    feat: add likes Dispatch in likes handler

--- src/utils/likes-handler.js
+++ src/utils/likes-handler.js
@@ -1,10 +1,22 @@
 import { likeService } from '../services';
 
-export const likesHandler = async (token, video) => {
+export const likesHandler = async ({
+	setBtnLoading,
+	token,
+	video,
+	likesDispatch,
+	setIsVisible,
+}) => {
+	setBtnLoading((prev) => ({ ...prev, likes: true }));
 	try {
 		const response = await likeService(token, video);
-		console.log(response);
+		if (response.status === 201) {
+			likesDispatch({ type: 'ADD_TO_LIKES', payload: response.data.likes });
+		}
 	} catch (error) {
 		console.error('ERROR: ', error.response);
+	} finally {
+		setBtnLoading((prev) => ({ ...prev, likes: false }));
+		setIsVisible(false);
 	}
 };
