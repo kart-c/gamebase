b9ba3054f3e266badd8ead422ac118d78bd4954c
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-22 11:26:30 +0530

    feat: add likes context

---
+++ src/context/LikesContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext, useReducer } from 'react';
+import { likesReducer } from '../reducer';
+
+const LikesContext = createContext({ likesState: { likes: [] }, likesDispatch: () => {} });
+
+const LikesProvider = ({ children }) => {
+	const [likesState, likesDispatch] = useReducer(likesReducer, { likes: [] });
+	return (
+		<LikesContext.Provider value={{ likesState, likesDispatch }}>{children}</LikesContext.Provider>
+	);
+};
+
+const useLikes = () => useContext(LikesContext);
+
+export { LikesProvider, useLikes };
