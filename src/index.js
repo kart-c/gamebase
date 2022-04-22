9fd0c3f3ea3da7d127da6d13f5ed4afec2e6cfc8
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-22 11:27:18 +0530

    chore: add likes provider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { AuthProvider } from './context';
+import { AuthProvider, LikesProvider } from './context';
 import { makeServer } from './server';
 
 // Call make Server
@@ -11,9 +11,11 @@ makeServer();
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<AuthProvider>
-				<App />
-			</AuthProvider>
+			<LikesProvider>
+				<AuthProvider>
+					<App />
+				</AuthProvider>
+			</LikesProvider>
 		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
