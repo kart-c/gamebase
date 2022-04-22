2a7f56c6b169367784694ecd48b94932ac0bdbed
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-22 15:08:38 +0530

    feat: add options toggle

--- src/components/HorizontalCard/HorizontalCard.jsx
+++ src/components/HorizontalCard/HorizontalCard.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './HorizontalCard.module.css';
 
 const HorizontalCard = ({ videoThumbnail, title, channelName, time }) => {
+	const [isVisible, setIsVisible] = useState(false);
+
+	const optionsHandler = () => setIsVisible((prev) => !prev);
+
 	return (
 		<article className={styles.hrCard}>
+			{isVisible && (
+				<div className={styles.menu}>
+					<button>Remove from likes</button>
+				</div>
+			)}
 			<div className={styles.imgContainer}>
 				<img src={videoThumbnail} alt={title} className="resp-img" />
 				<span>{time}</span>
@@ -12,7 +21,7 @@ const HorizontalCard = ({ videoThumbnail, title, channelName, time }) => {
 				<h4 className={styles.title}>{title}</h4>
 				<h5>{channelName}</h5>
 			</div>
-			<button className={`btn-btn-primary ${styles.options}`}>
+			<button className={`btn-btn-primary ${styles.options}`} onClick={optionsHandler}>
 				<i className="fa-solid fa-ellipsis-vertical"></i>
 			</button>
 		</article>
