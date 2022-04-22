124c8ef22ac8ea5fb06132e49cf9008defeb4100
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-22 15:02:48 +0530

    feat: add options button

--- src/components/HorizontalCard/HorizontalCard.jsx
+++ src/components/HorizontalCard/HorizontalCard.jsx
@@ -12,6 +12,9 @@ const HorizontalCard = ({ videoThumbnail, title, channelName, time }) => {
 				<h4 className={styles.title}>{title}</h4>
 				<h5>{channelName}</h5>
 			</div>
+			<button className={`btn-btn-primary ${styles.options}`}>
+				<i className="fa-solid fa-ellipsis-vertical"></i>
+			</button>
 		</article>
 	);
 };
