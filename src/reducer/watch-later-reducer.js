3981071a3190e61d393ebf737a6e92304d9536e3
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-23 18:43:14 +0530

    feat: delete from watchlater case in reducer

--- src/reducer/watch-later-reducer.js
+++ src/reducer/watch-later-reducer.js
@@ -3,6 +3,9 @@ export const watchLaterReducer = (state, { type, payload }) => {
 		case 'ADD_TO_WATCHLATER':
 			return { ...state, watchlater: payload };
 
+		case 'REMOVE_FROM_WATCHLATER':
+			return { ...state, watchlater: payload };
+
 		default:
 			throw new Error('NO CASE DEFINED IN WATCH LATER REDUCER');
 	}
