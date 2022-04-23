578cc578ba78b4714c7f18954f5838a74ab74d57
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-23 17:52:08 +0530

    feat: add to watchlater service

--- src/services/index.js
+++ src/services/index.js
@@ -3,3 +3,4 @@ export { authService } from './auth.service';
 export { likeService } from './like.service';
 export { likeDeleteService } from './like-delete.service';
 export { fetchLikes } from './fetch-likes.service';
+export { watchLaterService } from './add-to-wl.service';
