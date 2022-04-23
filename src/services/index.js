2541f55bc86b1c0e18a9e232f4b8a91fd6441bf0
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-23 18:43:02 +0530

    feat: delete from watchlater service

--- src/services/index.js
+++ src/services/index.js
@@ -4,3 +4,4 @@ export { likeService } from './like.service';
 export { likeDeleteService } from './like-delete.service';
 export { fetchLikes } from './fetch-likes.service';
 export { watchLaterService } from './add-to-wl.service';
+export { watchLaterDelete } from './wl-delete.service';
