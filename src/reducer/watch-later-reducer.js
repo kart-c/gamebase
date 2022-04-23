6281ab106c3b759e8708beacb6d64f557c287f3b
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-23 17:52:20 +0530

    feat: add to watchlater case in reducer

--- src/reducer/watch-later-reducer.js
+++ src/reducer/watch-later-reducer.js
@@ -1,5 +1,8 @@
 export const watchLaterReducer = (state, { type, payload }) => {
 	switch (type) {
+		case 'ADD_TO_WATCHLATER':
+			return { ...state, watchlater: payload };
+
 		default:
 			throw new Error('NO CASE DEFINED IN WATCH LATER REDUCER');
 	}
