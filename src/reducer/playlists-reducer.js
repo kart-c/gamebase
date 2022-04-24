a8e35d4e4416bc13c024e125857c236025fe6c39
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-24 22:04:18 +0530

    refactor: remove console.logs

--- src/reducer/playlists-reducer.js
+++ src/reducer/playlists-reducer.js
@@ -1,7 +1,6 @@
 export const playlistsReducer = (state, { type, payload }) => {
 	switch (type) {
 		case 'ADD_NEW_PLAYLIST':
-			console.log(payload);
 			return { ...state, playlists: payload };
 
 		default:
