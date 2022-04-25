a1f214027cee1a910f373ca29504037ae2c6caca
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-25 20:41:26 +0530

    fix: fix typo

--- src/utils/get-playlist.js
+++ src/utils/get-playlist.js
@@ -4,7 +4,7 @@ export const getPlaylist = async (token, playlistsDispatch) => {
 	try {
 		const response = await getPlaylistService(token);
 		if (response.status === 200) {
-			playlistsDispatch({ type: 'GET_PLAYLISTS', playload: response.data.playlists });
+			playlistsDispatch({ type: 'GET_PLAYLISTS', payload: response.data.playlists });
 		}
 	} catch (error) {
 		console.error('ERROR: ', error.response);
