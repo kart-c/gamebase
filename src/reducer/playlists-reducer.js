1efbee7fec974930c499b016d197384e510bcff1
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-25 14:14:16 +0530

    feat: add to playlist case in reducer

--- src/reducer/playlists-reducer.js
+++ src/reducer/playlists-reducer.js
@@ -3,6 +3,13 @@ export const playlistsReducer = (state, { type, payload }) => {
 		case 'ADD_NEW_PLAYLIST':
 			return { ...state, playlists: payload };
 
+		case 'ADD_NEW_VIDEO':
+			const newPlaylists = (playlists) =>
+				playlists.map((playlist) =>
+					playlist._id === payload._id ? { ...playlist, videos: payload.videos } : playlist
+				);
+			return { ...state, playlists: newPlaylists(state.playlists) };
+
 		default:
 			throw new Error('NO CASE DEFINED IN PLAYLISTS REDUCER');
 	}
