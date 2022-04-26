2efecb850a82e7fa86b4a1b828865ba04dde3179
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-26 15:18:52 +0530

    refactor: refactor playlist card

--- src/components/PlaylistCard/PlaylistCard.jsx
+++ src/components/PlaylistCard/PlaylistCard.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { empty } from '../../assets';
 import styles from './PlaylistCard.module.css';
 
-const PlaylistCard = () => {
+const PlaylistCard = ({ title, playlistImg, description, videos }) => {
+	const names = videos.reduce(
+		(acc, curr) => (videos.length === 1 ? acc + curr.title : (acc += curr.title + ', ')),
+		''
+	);
+	const text = description
+		? description
+		: videos.length > 0
+		? `This playlist includes ${names}`
+		: 'Empty Playlist';
 	return (
 		<article className={styles.playlist}>
-			<h3>Playlist name</h3>
-			<div className={styles.iconContainer}>
+			<div className={styles.playlistDescription}>
+				<h3>{title}</h3>
+				<p>{text}</p>
+			</div>
+			<div className={styles.imageContainer}>
+				<img src={playlistImg ?? empty} alt={title} className="resp-img" />
 				<i className="fa-solid fa-list-check"></i>
 			</div>
 		</article>
