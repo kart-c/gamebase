a4db8881e0ad75871e77cb3a330ba97e4a7bb36f
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-27 17:36:08 +0530

    feat: add channel description and video description

--- src/pages/SingleVideo/SingleVideo.jsx
+++ src/pages/SingleVideo/SingleVideo.jsx
@@ -7,6 +7,7 @@ import styles from './SingleVideo.module.css';
 const SingleVideo = () => {
 	const [currentVideo, setCurrentVideo] = useState(null);
 	const [isLoading, setIsLoading] = useState(true);
+	const [showMore, setShowMore] = useState(false);
 	const params = useParams();
 
 	useEffect(() => {
@@ -20,34 +21,61 @@ const SingleVideo = () => {
 			{isLoading ? (
 				'Loading'
 			) : (
-				<div className={styles.videoWrapper}>
-					<ReactPlayer
-						url={`https://www.youtube.com/watch?v=${currentVideo._id}`}
-						controls
-						height="100%"
-						width="100%"
-					/>
-					<div className={styles.detailContainer}>
-						<h4>{currentVideo.title}</h4>
-						<div className={styles.videoDetails}>
-							<span>{currentVideo.viewCount}</span>
-							<div className={styles.btnContainer}>
-								<button className="btn btn-primary">
-									Like
-									<i className="fa-solid fa-thumbs-up"></i>
-								</button>
-								<button className="btn btn-primary">
-									Watch Later
-									<i className="fa-solid fa-clock"></i>
-								</button>
-								<button className="btn btn-primary">
-									Playlist
-									<i className="fa-solid fa-list-ul"></i>
-								</button>
+				<>
+					<div className={styles.videoWrapper}>
+						<ReactPlayer
+							url={`https://www.youtube.com/watch?v=${currentVideo._id}`}
+							controls
+							height="40rem"
+							width="100%"
+						/>
+						<div className={styles.detailContainer}>
+							<h4>{currentVideo.title}</h4>
+							<div className={styles.videoDetails}>
+								<span>{currentVideo.viewCount}</span>
+								<div className={styles.btnContainer}>
+									<button className="btn btn-primary">
+										Like
+										<i className="fa-solid fa-thumbs-up"></i>
+									</button>
+									<button className="btn btn-primary">
+										Watch Later
+										<i className="fa-solid fa-clock"></i>
+									</button>
+									<button className="btn btn-primary">
+										Playlist
+										<i className="fa-solid fa-list-ul"></i>
+									</button>
+								</div>
+							</div>
+						</div>
+						<div className={styles.channelDetailContainer}>
+							<img
+								src={currentVideo.channelThumbnail}
+								alt={currentVideo.channelName}
+								className="avatar avatar-sm"
+							/>
+							<div className={styles.channelDetails}>
+								<h4>{currentVideo.channelName}</h4>
+								<p className={styles.description}>
+									{currentVideo.description.substring(
+										0,
+										currentVideo.description.length > 150
+											? showMore
+												? currentVideo.description.length
+												: 150
+											: currentVideo.description.length
+									)}
+									{currentVideo.description.length > 150 && (
+										<button onClick={() => setShowMore((prev) => !prev)}>
+											{showMore ? 'Show Less' : 'Show More'}
+										</button>
+									)}
+								</p>
 							</div>
 						</div>
 					</div>
-				</div>
+				</>
 			)}
 		</div>
 	);
