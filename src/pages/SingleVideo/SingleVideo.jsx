ab2f30c6f9b6dba0337d18d8d982c6d51d531b23
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-27 16:15:40 +0530

    feat: add video and details on single video page

--- src/pages/SingleVideo/SingleVideo.jsx
+++ src/pages/SingleVideo/SingleVideo.jsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player/youtube';
+import { useParams } from 'react-router-dom';
+import { getVideo } from '../../utils';
+import styles from './SingleVideo.module.css';
 
 const SingleVideo = () => {
-	return <div>SingleVideo</div>;
+	const [currentVideo, setCurrentVideo] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
+	const params = useParams();
+
+	useEffect(() => {
+		getVideo(params._id, setCurrentVideo, setIsLoading);
+	}, []);
+
+	console.log(currentVideo);
+
+	return (
+		<div className={styles.pg}>
+			{isLoading ? (
+				'Loading'
+			) : (
+				<div className={styles.videoWrapper}>
+					<ReactPlayer
+						url={`https://www.youtube.com/watch?v=${currentVideo._id}`}
+						controls
+						height="100%"
+						width="100%"
+					/>
+					<div className={styles.detailContainer}>
+						<h4>{currentVideo.title}</h4>
+						<div className={styles.videoDetails}>
+							<span>{currentVideo.viewCount}</span>
+							<div className={styles.btnContainer}>
+								<button className="btn btn-primary">
+									Like
+									<i className="fa-solid fa-thumbs-up"></i>
+								</button>
+								<button className="btn btn-primary">
+									Watch Later
+									<i className="fa-solid fa-clock"></i>
+								</button>
+								<button className="btn btn-primary">
+									Playlist
+									<i className="fa-solid fa-list-ul"></i>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export { SingleVideo };
