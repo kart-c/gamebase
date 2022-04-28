5630984b011a935ba301b3e7cf94dab652d19120
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-28 22:22:53 +0530

    feat: add delete all history button

--- src/components/PageDetails/PageDetails.jsx
+++ src/components/PageDetails/PageDetails.jsx
@@ -5,17 +5,26 @@ import styles from './PageDetails.module.css';
 const PageDetails = ({ pg, length, video, date }) => {
 	const formattedDate = (date) => new Date(date).toDateString();
 
+	const deleteHandler = () => {};
+
 	return (
 		<section className={styles.pageDetails}>
 			<div className={styles.imgContainer}>
 				<img src={video ? video.videoThumbnail : empty} alt="liked page" className="resp-img" />
-				<button className="btn btn-primary">Play</button>
+				{pg !== 'History' && <button className="btn btn-primary">Play</button>}
 			</div>
 			<h3 className={styles.title}>{pg} Videos</h3>
 			<div className={styles.description}>
 				<span>{length ? `${length} ${length === 1 ? 'Video' : 'Videos'} ` : 'No Videos'}</span>
-				{date && <span className={styles.date}>Last Updated on {formattedDate(date)}</span>}
+				{date && length ? (
+					<span className={styles.date}>Last Updated on {formattedDate(date)}</span>
+				) : null}
 			</div>
+			{length ? (
+				<button className={`btn btn-primary ${styles.deleteBtn}`} onClick={deleteHandler}>
+					Delete all history
+				</button>
+			) : null}
 		</section>
 	);
 };
