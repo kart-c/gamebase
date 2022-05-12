4626a729e33bd41194f1ef2b09e494a5af75c62a
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-12 15:36:51 +0530

    feat: add toast on likes

--- src/utils/delete-like.js
+++ src/utils/delete-like.js
@@ -1,3 +1,4 @@
+import { toast } from 'react-toastify';
 import { likeDeleteService } from '../services';
 
 export const deleteLike = async ({ _id, token, likesDispatch, setBtnLoading, setIsVisible }) => {
@@ -5,13 +6,15 @@ export const deleteLike = async ({ _id, token, likesDispatch, setBtnLoading, set
 	try {
 		const response = await likeDeleteService(_id, token);
 		if (response.status === 200) {
+			toast.success('Video removed from likes');
 			likesDispatch({
 				type: 'REMOVE_FROM_LIKES',
 				payload: { likes: response.data.likes, date: response.data.date },
 			});
 		}
 	} catch (error) {
-		console.error('ERROR: ', error.response);
+		toast.error(error.response.data.errors[0]);
+		console.error('ERROR: ', error);
 	} finally {
 		setBtnLoading((prev) => ({ ...prev, likes: false }));
 		setIsVisible(false);
