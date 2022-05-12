847935cc7d50796582887c7d351f113b7ab0f1db
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-12 17:18:48 +0530

    feat: toast on playlist delete

--- src/utils/delete-playlist-handler.js
+++ src/utils/delete-playlist-handler.js
@@ -1,3 +1,4 @@
+import { toast } from 'react-toastify';
 import { deletePlaylistService } from '../services';
 
 export const deletePlaylist = async ({ _id, token, playlistsDispatch, setBtnLoader }) => {
@@ -5,10 +6,12 @@ export const deletePlaylist = async ({ _id, token, playlistsDispatch, setBtnLoad
 	try {
 		const response = await deletePlaylistService(_id, token);
 		if (response.status === 200) {
+			toast.success('Deleted playlist');
 			playlistsDispatch({ type: 'REMOVE_PLAYLIST', payload: response.data.playlists });
 		}
 	} catch (error) {
-		console.error('ERROR: ', error.response);
+		toast.error(error.response.data.errors[0]);
+		console.error('ERROR: ', error);
 	} finally {
 		setBtnLoader(false);
 	}
