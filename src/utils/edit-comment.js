8880650c30d91ef3f986fb8e5f7f27ea22e65567
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-25 11:05:08 +0530

    feat: edit comment functionallity

---
+++ src/utils/edit-comment.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const editComment = async (_id, token, note, commentDispatch) => {
+	try {
+		const response = await axios.post(
+			`/api/user/notes/update/${_id}`,
+			{ note },
+			{ headers: { authorization: token } }
+		);
+		if (response.status === 200) {
+			commentDispatch({ type: 'EDIT_COMMENT', payload: response.data.notes });
+		}
+		console.log(response);
+	} catch (error) {
+		console.error('ERROR', error);
+	}
+};
