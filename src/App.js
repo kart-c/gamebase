0c9ca9c4aecb740c61f2ec86763eabdfabe129aa
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-29 10:22:48 +0530

    refactor: show toast at bottom right

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 	return (
 		<>
 			<ToastContainer
-				position="top-right"
+				position="bottom-right"
 				autoClose={2000}
 				hideProgressBar={false}
 				newestOnTop={false}
