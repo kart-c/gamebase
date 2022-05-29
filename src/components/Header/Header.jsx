74e9797959bad98e72df636e21601775d1f9041c
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-29 14:05:55 +0530

    feat: add search debouncing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useAuth, useLikes, usePlaylists, useWatchLater } from '../../context';
 import styles from './Header.module.css';
 
-const Header = ({ searchInput, setSearchInput }) => {
+const Header = ({ videos }) => {
+	const [searchInput, setSearchInput] = useState('');
+	const [searchList, setSearchList] = useState([]);
+	const [isDebouncing, setIsDebouncing] = useState(false);
+	const timerRef = useRef();
+	const navigate = useNavigate();
 	const {
 		authState: { token },
 		authDispatch,
@@ -24,25 +29,58 @@ const Header = ({ searchInput, setSearchInput }) => {
 		watchLaterDispatch({ type: 'CLEAR' });
 	};
 
-	const inputHandler = (e) => {
-		setSearchInput(e.target.value);
-	};
+	useEffect(() => {
+		if (searchInput) {
+			clearTimeout(timerRef.current);
+			if (searchInput !== 0) {
+				setIsDebouncing(false);
+				timerRef.current = setTimeout(() => {
+					const searchedProducts = videos.filter(
+						(video) =>
+							video.title.toLowerCase().includes(searchInput.toLowerCase()) ||
+							video.channelName.toLowerCase().includes(searchInput.toLowerCase())
+					);
+					setSearchList(searchedProducts);
+					setIsDebouncing(true);
+				}, 300);
+			}
+		}
+	}, [searchInput]);
 
 	return (
 		<header className={`header ${styles.header}`}>
 			<h2>
 				<Link to="/">GAMEBASE</Link>
 			</h2>
-			{location.pathname === '/' && (
-				<input
-					type="search"
-					name="search"
-					className="header-search"
-					placeholder="Search..."
-					value={searchInput}
-					onChange={inputHandler}
-				/>
-			)}
+			<div className={styles.searchContainer}>
+				{location.pathname === '/' && (
+					<input
+						type="search"
+						name="search"
+						className="header-search"
+						placeholder="Search..."
+						value={searchInput}
+						onChange={(e) => setSearchInput(e.target.value)}
+					/>
+				)}
+				{searchInput && isDebouncing ? (
+					<ul className={styles.searchList}>
+						{searchList.length > 0
+							? searchList.map((listItem) => (
+									<li
+										key={listItem._id}
+										onClick={() => {
+											navigate(`/explore/${listItem._id}`);
+											setSearchValue('');
+										}}
+									>
+										{listItem.title}
+									</li>
+							  ))
+							: 'No products found'}
+					</ul>
+				) : null}
+			</div>
 			{token ? (
 				<button onClick={logoutHandler} className={styles.logoutBtn}>
 					Logout
