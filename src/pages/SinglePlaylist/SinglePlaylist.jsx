dcc911fb958ac53cc8cf635f557343f881c27e8a
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-06-07 11:41:23 +0530

    navigate to 404 if error on single playlist page

--- src/pages/SinglePlaylist/SinglePlaylist.jsx
+++ src/pages/SinglePlaylist/SinglePlaylist.jsx
@@ -1,38 +1,47 @@
-import { Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+import { Fragment, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { HorizontalCard, HrCardWrapper, PageDetails } from '../../components';
 import { usePlaylists } from '../../context';
 import styles from './SinglePlaylist.module.css';
 
 const SinglePlaylist = () => {
 	const params = useParams();
+	const navigate = useNavigate();
 	const {
 		playlistsState: { playlists, date },
 	} = usePlaylists();
 
 	const currentPlaylist = playlists.find((playlist) => playlist._id === params._id);
 
+	useEffect(() => {
+		if (!currentPlaylist) navigate('*');
+	}, [params, playlists]);
+
 	return (
 		<div className={`pg-defaults ${styles.pgContainer}`}>
-			<PageDetails
-				pg={currentPlaylist.title}
-				length={currentPlaylist.videos.length}
-				video={currentPlaylist.videos.length ? currentPlaylist.videos[0] : null}
-				date={date}
-			/>
-			<HrCardWrapper>
-				{currentPlaylist.videos.length > 0 ? (
-					currentPlaylist.videos.map((video) => (
-						<Fragment key={video._id}>
-							<HorizontalCard {...video} playlist_id={currentPlaylist._id} />
-						</Fragment>
-					))
-				) : (
-					<div className={styles.noVideos}>
-						<h4>You do not have videos in this playlist</h4>
-					</div>
-				)}
-			</HrCardWrapper>
+			{currentPlaylist ? (
+				<>
+					<PageDetails
+						pg={currentPlaylist.title}
+						length={currentPlaylist.videos.length}
+						video={currentPlaylist.videos.length ? currentPlaylist.videos[0] : null}
+						date={date}
+					/>
+					<HrCardWrapper>
+						{currentPlaylist.videos.length > 0 ? (
+							currentPlaylist.videos.map((video) => (
+								<Fragment key={video._id}>
+									<HorizontalCard {...video} playlist_id={currentPlaylist._id} />
+								</Fragment>
+							))
+						) : (
+							<div className={styles.noVideos}>
+								<h4>You do not have videos in this playlist</h4>
+							</div>
+						)}
+					</HrCardWrapper>
+				</>
+			) : null}
 		</div>
 	);
 };
